710f8168ba588e489e9ac68a060eb8b5b3257d96
Rajat Nagpal <48415329+thisisrajatnagpal@users.noreply.github.com>
2023-05-01 23:42:19 +0530

    Create atr.py

---
+++ Python Functions/atr.py
@@ -0,0 +1,9 @@
+def atr(DF,n):
+    "function to calculate True Range and Average True Range"
+    df = DF.copy()
+    df['H-L']=abs(df['high']-df['low'])
+    df['H-PC']=abs(df['high']-df['close'].shift(1))
+    df['L-PC']=abs(df['low']-df['close'].shift(1))
+    df['TR']=df[['H-L','H-PC','L-PC']].max(axis=1,skipna=False)
+    df['ATR'] = df['TR'].ewm(com=n,min_periods=n).mean()
+    return df['ATR']
