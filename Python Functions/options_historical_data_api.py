2365db2f89cf337cb8c3815af1f50b185189efbb
Rajat Nagpal <48415329+thisisrajatnagpal@users.noreply.github.com>
2023-05-16 20:32:24 +0530

    Update options_historical_data_api.py

--- Python Functions/options_historical_data_api.py
+++ Python Functions/options_historical_data_api.py
@@ -6,14 +6,3 @@ ma.login("password")
 # ins is the name if the instrument, i.e., "banknifty" or "nifty"
 options_data = ma.get_data(ins, timestamp.date())
 
-
-# This function generates the symbol for the desired strike price based on above generated data
-
-# Here, price is the strike price you want to use
-# Call_put is a string,  it can take value "CE" or "PE". Default is "CE"
-def get_options_symbol(options_data, price, call_put = "CE"):
-    substr = str(price) + call_put
-    for iter in options_data['symbol']:
-      # To check the presence of a substring in a string
-        if(substr in iter):
-            return iter
