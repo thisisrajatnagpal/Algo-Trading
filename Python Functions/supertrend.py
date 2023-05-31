08a48f3c3a19b027b8cc52ce240cd7b0992e580f
Rajat Nagpal <48415329+thisisrajatnagpal@users.noreply.github.com>
2023-05-31 12:17:52 +0530

    Create supertrend.py

---
+++ Python Functions/supertrend.py
@@ -0,0 +1,40 @@
+def supertrend(DF,n,m):
+    # Here, DF refers to the OHLC DataFrame
+    """function to calculate Supertrend given historical candle data
+        n = n day ATR - usually 7 day ATR is used
+        m = multiplier - usually 2 or 3 is used"""
+    df = DF.copy()
+    df['ATR'] = atr(df,n)
+    df["B-U"]=((df['high']+df['low'])/2) + m*df['ATR'] 
+    df["B-L"]=((df['high']+df['low'])/2) - m*df['ATR']
+    df["U-B"]=df["B-U"]
+    df["L-B"]=df["B-L"]
+    ind = df.index
+    for i in range(n,len(df)):
+        if df['close'][i-1]<=df['U-B'][i-1]:
+            df.loc[ind[i],'U-B']=min(df['B-U'][i],df['U-B'][i-1])
+        else:
+            df.loc[ind[i],'U-B']=df['B-U'][i]    
+    for i in range(n,len(df)):
+        if df['close'][i-1]>=df['L-B'][i-1]:
+            df.loc[ind[i],'L-B']=max(df['B-L'][i],df['L-B'][i-1])
+        else:
+            df.loc[ind[i],'L-B']=df['B-L'][i]  
+    df['Strend']=np.nan
+    for test in range(n,len(df)):
+        if df['close'][test-1]<=df['U-B'][test-1] and df['close'][test]>df['U-B'][test]:
+            df.loc[ind[test],'Strend']=df['L-B'][test]
+            break
+        if df['close'][test-1]>=df['L-B'][test-1] and df['close'][test]<df['L-B'][test]:
+            df.loc[ind[test],'Strend']=df['U-B'][test]
+            break
+    for i in range(test+1,len(df)):
+        if df['Strend'][i-1]==df['U-B'][i-1] and df['close'][i]<=df['U-B'][i]:
+            df.loc[ind[i],'Strend']=df['U-B'][i]
+        elif  df['Strend'][i-1]==df['U-B'][i-1] and df['close'][i]>=df['U-B'][i]:
+            df.loc[ind[i],'Strend']=df['L-B'][i]
+        elif df['Strend'][i-1]==df['L-B'][i-1] and df['close'][i]>=df['L-B'][i]:
+            df.loc[ind[i],'Strend']=df['L-B'][i]
+        elif df['Strend'][i-1]==df['L-B'][i-1] and df['close'][i]<=df['L-B'][i]:
+            df.loc[ind[i],'Strend']=df['U-B'][i]
+    return df['Strend']
