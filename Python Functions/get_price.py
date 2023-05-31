1b20f80ffec083021de95703bc628c3260c15871
Rajat Nagpal <48415329+thisisrajatnagpal@users.noreply.github.com>
2023-05-31 18:32:16 +0530

    Update get_price.py

--- Python Functions/get_price.py
+++ Python Functions/get_price.py
@@ -1,5 +1,5 @@
-def get_price(ohlc):
-    ref = ohlc['close'].iloc[-1]
+def get_price(ref):
+    # ref = ohlc['close'].iloc[-1]
     limit_price = ref*(1+0.02/100)
     sl_price = 0.93*limit_price
     return round(limit_price, 1), round(sl_price, 1)
