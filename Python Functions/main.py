e95f2bec5d63cdc308af0395292d7d4781c5329b
Rajat Nagpal <48415329+thisisrajatnagpal@users.noreply.github.com>
2023-09-30 19:02:39 +0530

    Update main.py

--- Python Functions/main.py
+++ Python Functions/main.py
@@ -21,3 +21,24 @@ def main(capital):
      for ticker in tickers:
         print("starting passthrough for.....",ticker)    
         try:
+            # last traded price of the NSE ticker stock
+            ltp = kite.ltp("NSE:" + ticker)["NSE:" + ticker]['last_price']
+
+            # currently quantity is set to one lot per options ticker, it can be changed in util_get_options_symbol function
+            ce_symbol, pe_symbol, quantity = util_get_options_symbol(ticker, ltp)
+            order_present = False
+            
+            if(pos_df.shape[0] != 0):
+                # if there is an open position
+                if(pos_df[pos_df['tradingsymbol'].str.contains(ticker)].shape[0] != 0):    
+                    order_present = True
+                
+
+            if (not(order_present)):
+            # Give some condition here
+                Place_SL_Order(ce_symbol, quantity, order_type)
+            if(order_present):
+                order_id = ord_df.loc[(ord_df['tradingsymbol'].str.contains(ticker)) & (ord_df['status'].isin(["TRIGGER PENDING","OPEN"]))]
+                Modify_Order(order_id, order_type)    
+        except:
+            print("API error for ticker :", ticker)
