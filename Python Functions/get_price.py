b890f185c9e4053ab27d9ce72faea8ab4577f5d4
Rajat Nagpal <48415329+thisisrajatnagpal@users.noreply.github.com>
2023-09-30 12:42:34 +0530

    Update get_price.py

--- Python Functions/get_price.py
+++ Python Functions/get_price.py
@@ -1,5 +1,9 @@
-def get_price(ref):
-    # ref = ohlc['close'].iloc[-1]
-    limit_price = ref*(1+0.02)
-    sl_price = 0.93*limit_price
-    return round(limit_price, 1), round(sl_price, 1)
+def get_price(ref, order_type = "buy"):
+    if(order_type == "buy"):
+        limit_price = ref*(1+0.005)
+        sl_price = 0.93*limit_price
+        return round(limit_price, 1), round(sl_price, 1)
+    elif(order_type == "sell"):
+        limit_price = ref*(1-0.005)
+        sl_price = 1.07*limit_price
+        return round(limit_price, 1), round(sl_price, 1)
