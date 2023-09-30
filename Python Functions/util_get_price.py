9653a928f93edc45b2055fcf19230a9dbcdfa212
Rajat Nagpal <48415329+thisisrajatnagpal@users.noreply.github.com>
2023-09-30 13:51:11 +0530

    Update and rename get_price.py to util_get_price.py

---
+++ Python Functions/util_get_price.py
@@ -0,0 +1,13 @@
+def util_get_price(ref, order_type = "buy", sl_per = 7):
+    if(order_type == "buy"):
+    # The order is a buy order, stop loss price is 7 percent less than the limit price
+        # We have to set the limit price a little higher than the market price to get the order executed
+        limit_price = ref*(1+0.005)
+        sl_price = (1-sl_per/100)*limit_price
+        return round(limit_price, 1), round(sl_price, 1)
+    elif(order_type == "sell"):
+    # The order is a sell order, stop loss price is 7 percent higher than the limit price
+        # We have to set the limit price a little lower to get the order executed
+        limit_price = ref*(1-0.005)
+        sl_price = (1+sl_per/100)*limit_price
+        return round(limit_price, 1), round(sl_price, 1)
