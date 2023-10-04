a9a4be996132419f517e495abc0c3204143862a3
Rajat Nagpal <48415329+thisisrajatnagpal@users.noreply.github.com>
2023-10-04 17:15:09 +0530

    Update main.py

--- Python Functions/main.py
+++ Python Functions/main.py
@@ -39,7 +39,7 @@ def main(capital):
 
             if (not(open_position)):
             # Give some condition here
-                Place_Order_with_SL(ce_symbol, quantity, order_type)
+                Place_Order_with_SL(ce_symbol, quantity, order_type, SL_per = 7)
             if(open_position):
                 order_df = ord_df.loc[(ord_df['tradingsymbol'].str.contains(ticker)) & (ord_df['status'].isin(["TRIGGER PENDING","OPEN"]))]
                 Modify_SL_Order(order_df, order_type)    
