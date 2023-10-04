e863ae6990055a96a24fbe218a21440a856e12f8
Rajat Nagpal <48415329+thisisrajatnagpal@users.noreply.github.com>
2023-10-04 17:17:51 +0530

    Update Modify_SL_Order.py

--- Python Functions/Modify_SL_Order.py
+++ Python Functions/Modify_SL_Order.py
@@ -1,4 +1,4 @@
-def Modify_SL_Order(order_df, order_type = "buy"):  
+def Modify_SL_Order(order_df, order_type = "buy", sl_per = 7):  
     # We have to get the options ticker in order to proceed with the function
     symbol = order_df['tradingsymbol'].iloc[0]
     # getting last traded price of the ticker from the API, please note that in order to get the last traded price of an options symbol, we have to add the prefix "NFO:"
@@ -6,7 +6,7 @@ def Modify_SL_Order(order_df, order_type = "buy"):
 
     # getting the stop loss based on the current traded price of the options ticker
     # leaving first variable out because we just need the stop loss price
-    _, new_sl_price = util_get_price(ltp, order_type)
+    _, new_sl_price = util_get_price(ltp, order_type, sl_per)
     if(order_type == "buy"):
         # If the order was a buy order, it means the trigger price for the stop loss order is going to be greater than the stop loss price
         tp = round((1+0.01)*new_sl_price, 1)
