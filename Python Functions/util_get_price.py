d042975ed07a0ecd96c132e052c7f5d28808ac76
Rajat Nagpal <48415329+thisisrajatnagpal@users.noreply.github.com>
2023-10-07 15:38:48 +0530

    Update util_get_price.py

--- Python Functions/util_get_price.py
+++ Python Functions/util_get_price.py
@@ -4,14 +4,19 @@ def util_get_price(ref, order_type = "buy", sl_per = 7, rr=None):
         # We have to set the limit price a little higher than the market price to get the order executed
         limit_price = ref*(1+0.005)
         sl_price = (1-sl_per/100)*limit_price
-        if(rr!=None): target_price = (1+rr*sl_per/100)*limit_price
+        if(rr!=None): 
+            target_price = (1+rr*sl_per/100)*limit_price
+            return round(limit_price, 1), round(sl_price, 1), round(target_price, 1)
         else: target_price = None
-        return round(limit_price, 1), round(sl_price, 1), round(target_price, 1)
+            return round(limit_price, 1), round(sl_price, 1), target_price
+        
     elif(order_type == "sell"):
     # The order is a sell order, stop loss price is 7 percent higher than the limit price
         # We have to set the limit price a little lower to get the order executed
         limit_price = ref*(1-0.005)
         sl_price = (1+sl_per/100)*limit_price
-        if(rr!=None): target_price = (1-rr*sl_per/100)*limit_price
+        if(rr!=None): 
+            target_price = (1-rr*sl_per/100)*limit_price
+            return round(limit_price, 1), round(sl_price, 1), round(target_price, 1)
         else: target_price = None
-        return round(limit_price, 1), round(sl_price, 1), round(target_price, 1)
+            return round(limit_price, 1), round(sl_price, 1), target_price
