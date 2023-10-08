52d4408a17a19ce15e741ef3522a1f345b752899
Rajat Nagpal <48415329+thisisrajatnagpal@users.noreply.github.com>
2023-10-08 18:40:23 +0530

    Update supertrend_main.py

--- Algorithms/supertrend_main.py
+++ Algorithms/supertrend_main.py
@@ -1,4 +1,39 @@
+
+
+# Importing required libraries
+from kiteconnect import KiteConnect
+import os
+import datetime as dt
+import pandas as pd
+import numpy as np
+import time
+import math
+import sys
+
+#cwd = os.chdir("D:\Algo Trading")
+sys.path.append("D:\Algo Trading")
+import modules
+import global_ as g
+# Giving the path to the current working directory
+
+# Generate trading session
+access_token = open("D:/Algo Trading/access_token.txt",'r').read()
+key_secret = open("D:/Algo Trading/api_key.txt",'r').read().split()
+kite = KiteConnect(api_key=key_secret[0])
+kite.set_access_token(access_token)
+Nifty_list = pd.read_csv("D:/Algo Trading/ind_nifty50list.csv")
+
+# Get dump of all NSE/NFO Instruments
+instrument_dump = kite.instruments("NSE")
+instrument_df = pd.DataFrame(instrument_dump)
+instrument_df = instrument_df[instrument_df['name'] != ""]
+instrument_df = instrument_df[instrument_df['tradingsymbol'].isin(Nifty_list['Symbol'].to_list())]
+NFO_instrument_df = pd.DataFrame(kite.instruments("NFO"))
+tickers = list(Nifty_list['Symbol'])
+
+
 def main(capital):
+    if(kite.margins()['equity']['available']['opening_balance'] - kite.margins()['equity']['available']['live_balance'] > capital): return
     a,b = 0,0
     while a < 10:
     # running the loop multiple times to increase the probability of getting the required information in case if connection is not eastablished in one go
@@ -18,23 +53,23 @@ def main(capital):
         except:
             print("can't extract order data..retrying")
             b+=1
-     for ticker in tickers:
+    for ticker in tickers:
         print("starting passthrough for.....",ticker)    
         try:
             global flag, ltp
-            ohlc = fetchOHLC(ticker,"5minute",4)
-            ohlc["st1"] = supertrend(ohlc,7,3)
-            ohlc["st2"] = supertrend(ohlc,10,3)
-            ohlc["st3"] = supertrend(ohlc,11,2)
+            ohlc = modules.fetchOHLC(ticker,"5minute",4)
+            ohlc["st1"] = modules.supertrend(ohlc,7,3)
+            ohlc["st2"] = modules.supertrend(ohlc,10,3)
+            ohlc["st3"] = modules.supertrend(ohlc,11,2)
 
             # find this function in "python functions" folder in the same repository
             # It checks whether the trend is reversed or not, i.e., it checks wether all three supertrends have changed colour
-            st_dir_refresh(ohlc, ticker)
-
-            #quantity = Using some notion, you have to decide how much quantity you want to trade for a particular stock based on the lot size for that particular stock
+            modules.st_dir_refresh(ohlc, ticker)
+            print(g.st_dir[ticker])
+            #quantity = # Using some notion, you have to decide how much quantity you want to trade for a particular stock based on the lot size for that particular stock
             ltp = kite.ltp("NSE:" + ticker)["NSE:" + ticker]['last_price']
 
-            ce_symbol, pe_symbol, quantity = get_options_symbol(ticker, ltp)
+            
             order_present = False
             
             if(pos_df.shape[0] != 0):
@@ -42,29 +77,28 @@ def main(capital):
                 if(pos_df[pos_df['tradingsymbol'].str.contains(ticker)].shape[0] != 0):    
                     order_present = True
                 
-
+            
             if (not(order_present)):
+                ce_symbol, pe_symbol, quantity = modules.get_options_symbol(ticker, ltp)
                 # the code will execute only if there is no open position for the particular tickers and symbols
-                if st_dir[ticker] == ["green","green","green"]:
-
-                    placeSLOrder(ce_symbol, quantity)
-                if st_dir[ticker] == ["red","red","red"]:
-                    placeSLOrder(pe_symbol, quantity)
+                if g.st_dir[ticker] == ["green","green","green"]:
+                    modules.placeSLOrder(ce_symbol, quantity)
+                if g.st_dir[ticker] == ["red","red","red"]:
+                    modules.placeSLOrder(pe_symbol, quantity)
             if(order_present):
-                order_id = ord_df.loc[(ord_df['tradingsymbol'].str.contains(ticker)) & (ord_df['status'].isin(["TRIGGER PENDING","OPEN"]))]
-                ModifyOrder(order_id)    
+                order_df = ord_df.loc[(ord_df['tradingsymbol'].str.contains(ticker)) & (ord_df['status'].isin(["TRIGGER PENDING","OPEN"]))]
+                modules.ModifyOrder(order_df)    
         except:
             print("API error for ticker :",ticker)
     
-tickers = # give the list of tickers here
 
 # tickers to track - recommended to use max movers from previous day
-capital = 15000 # position size
-st_dir = {} # directory to store super trend status for each ticker
+capital = 50000 # position size
+#g.st_dir = {} # directory to store super trend status for each ticker
 for ticker in tickers:
-    st_dir[ticker] = ["None" ,"None", "None"]      
+    g.st_dir[ticker] = ["None" ,"None", "None"]      
 starttime=time.time()
-timeout = time.time() + 60*60*2  # 60 seconds times 360 meaning 6 hrs
+timeout = pd.Timestamp(dt.date.today()) + pd.Timedelta('0 days 15:10:0.0000')
 x = pd.Timestamp(dt.date.today()) + pd.Timedelta('0 days 09:20:0.0000')
 x = dt.datetime.strptime(str(x), "%Y-%m-%d %H:%M:%S")
 while time.time() <= timeout:
